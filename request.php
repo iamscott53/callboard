eb4b3258ec4e89cc3f5afc4fcbb64fa00a83077c
trugenx <69480158+trugenx@users.noreply.github.com>
2020-11-16 08:41:53 -0500

    Face Sheets
    Updated to relfect facesheets

--- request.php
+++ request.php
@@ -2,8 +2,9 @@
 if($_POST) {
     include 'database.php'; // Ctreated by Scott Smith
     $db = new Database();
-    $db->insert("INSERT INTO kit_requests(name, request, date) VALUES(:name, :request, :date) ON DUPLICATE KEY UPDATE request = :request", ['name' => $_POST['name'], 'request' => $_POST['request'], 'date' => $_POST['date']]);
-    echo "Data has updated successfully.";
+    $table = ($_POST['campaign'] == 2) ? "kit_requests_campaign_2" : "kit_requests";
+    $db->insert("INSERT INTO ".$table."(name, request, date) VALUES(:name, :request, :date) ON DUPLICATE KEY UPDATE request = :request", ['name' => $_POST['name'], 'request' => $_POST['request'], 'date' => $_POST['date']]);
+    echo $_POST["campaign"];
 } else {
     echo "Failed to update call data.";
 }
