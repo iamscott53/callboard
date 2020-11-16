00b7f8ffce640288c51c9d5031382740d69f9104
trugenx <69480158+trugenx@users.noreply.github.com>
2020-11-16 09:19:56 -0500

    facesheet update
    update

--- delivery.php
+++ delivery.php
@@ -2,8 +2,9 @@
 if($_POST) {
     include 'database.php'; // Ctreated by Scott Smith
     $db = new Database();
-    $db->insert("INSERT INTO kit_delivered(name, quantity, date) VALUES(:name, :quantity, :date) ON DUPLICATE KEY UPDATE quantity = :quantity", ['name' => $_POST['name'], 'quantity' => $_POST['quantity'], 'date' => $_POST['date']]);
-    echo "Data has updated successfully.";
+    $table = ($_POST['campaign'] == 2) ? "kit_delivered_campaign_2" : "kit_delivered";
+    $db->insert("INSERT INTO ".$table."(name, quantity, date) VALUES(:name, :quantity, :date) ON DUPLICATE KEY UPDATE quantity = :quantity", ['name' => $_POST['name'], 'quantity' => $_POST['quantity'], 'date' => $_POST['date']]);
+    echo $_POST["campaign"];
 } else {
     echo "Failed to update call data.";
 }
