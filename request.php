691f204b0a07620198cd36719563a15b5119fa83
trugenx <69480158+trugenx@users.noreply.github.com>
2020-11-16 08:54:39 -0500

    Revert Changes
    Revert facesheet changes

--- request.php
+++ request.php
@@ -2,9 +2,8 @@
 if($_POST) {
     include 'database.php'; // Ctreated by Scott Smith
     $db = new Database();
-    $table = ($_POST['campaign'] == 2) ? "kit_requests_campaign_2" : "kit_requests";
-    $db->insert("INSERT INTO ".$table."(name, request, date) VALUES(:name, :request, :date) ON DUPLICATE KEY UPDATE request = :request", ['name' => $_POST['name'], 'request' => $_POST['request'], 'date' => $_POST['date']]);
-    echo $_POST["campaign"];
+    $db->insert("INSERT INTO kit_requests(name, request, date) VALUES(:name, :request, :date) ON DUPLICATE KEY UPDATE request = :request", ['name' => $_POST['name'], 'request' => $_POST['request'], 'date' => $_POST['date']]);
+    echo "Data has updated successfully.";
 } else {
     echo "Failed to update call data.";
 }
