cad77eaccbe4b89ad26a79419bd11915e2de41b4
trugenx <69480158+trugenx@users.noreply.github.com>
2021-05-17 08:53:29 -0400

    undo Kirsten

--- changecallboard.php
+++ changecallboard.php
@@ -185,6 +185,29 @@ if(!$permission) {
                             </div>
                         </td> <!-- Delivered Kits -->
                     </tr>
+                    <tr>
+                        <td>kirsten</td>
+                        <?php foreach($days as $day) { ?>
+                            <td>
+                                <div class="row">
+                                    <div class="col"><input type="number" value="<?php echo get_request_qty('kirsten', date('Y-m-d', strtotime(''.$day.' this week')), 1); ?>" name="<?php echo $day; ?>[]" data-name="kirsten" data-date="<?php echo date('Y-m-d', strtotime(''.$day.' this week')); ?>" data-campaign="1"></div>
+                                    <div class="col border-left"><input type="number" value="<?php echo get_request_qty('kirsten', date('Y-m-d', strtotime(''.$day.' this week')), 2); ?>" name="<?php echo $day; ?>[]" data-name="kirsten" data-date="<?php echo date('Y-m-d', strtotime(''.$day.' this week')); ?>" data-campaign="2"></div>
+                                </div>
+                            </td>
+                        <?php } ?>
+                        <td data-request="total">
+                            <div class="row">
+                                <div class="col"><?php echo get_total_request_by('kirsten', 1); ?></div>
+                                <div class="col border-left"><?php echo get_total_request_by('kirsten', 2); ?></div>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="row">
+                                <div class="col"><input type="number" value="<?php echo get_delivered_qty('kirsten', 1); ?>" name="delivered[]" data-name="kirsten" data-date="<?php echo date('Y-m-d'); ?>" data-campaign="1"></div>
+                                <div class="col border-left"><input type="number" value="<?php echo get_delivered_qty('kirsten', 2); ?>" name="delivered[]" data-name="kirsten" data-date="<?php echo date('Y-m-d'); ?>" data-campaign="2"></div>
+                            </div>
+                        </td> <!-- Delivered Kits -->
+                    </tr>
 					
 					<tr>
                         <td>victoria</td>
